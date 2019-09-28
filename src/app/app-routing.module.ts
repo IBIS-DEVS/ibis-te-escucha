7fd6e6978e1e99de8960493c81210d81dd5ffa9c
Jess <Unal>
2019-09-28 15:22:02 -0500

    add testimonial detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  { path: 'new-testimonial', loadChildren: './new-testimonial/new-testimonial.module#NewTestimonialPageModule' }
+  },  { path: 'new-testimonial', loadChildren: './new-testimonial/new-testimonial.module#NewTestimonialPageModule' },
+  { path: 'testimonial-detail', loadChildren: './testimonial-detail/testimonial-detail.module#TestimonialDetailPageModule' }
 
 ];
 @NgModule({
